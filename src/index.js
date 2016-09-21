41a1f377650031035e361f623d8d3d3a2299d405
Pattelicious <patrick.voss1808@gmail.com>
2016-09-21 20:04:33 +0200

    Add react-router

--- src/index.js
+++ src/index.js
@@ -8,12 +8,17 @@ import createLogger from 'redux-logger';
 import { Provider } from 'react-redux';
 import contacts from './reducers/ContactReducer';
 
+import { Router, Route, browserHistory } from 'react-router';
+import { hashHistory } from 'react-router'
+
 const logger = createLogger();
 const store = createStore(contacts, applyMiddleware(logger));
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router history={hashHistory}>
+      <Route path="/" component={App} />
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
